99ae3438b9d77740be25dca67904a5ad4a9517d3
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-01 12:24:05 +0200

    Added toolbar

--- app/src/main/java/com/apartmentslt/apartments/LoginActivity.java
+++ app/src/main/java/com/apartmentslt/apartments/LoginActivity.java
@@ -2,7 +2,9 @@ package com.apartmentslt.apartments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Appbar toolbar = new Appbar(this, R.id.toolbar, getTitle().toString());
+        toolbar.show();
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
