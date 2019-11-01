0e4d97db9700632590057577dd4c1d0bd517e080
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-01 12:23:12 +0200

    Created toolbar class

---
+++ app/src/main/java/com/apartmentslt/apartments/Appbar.java
@@ -0,0 +1,42 @@
+package com.apartmentslt.apartments;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class Appbar {
+    private Context context;
+    private Toolbar toolbar;
+
+    Appbar(Context context, int toolbarId, String title) {
+        this.context = context;
+
+        this.toolbar = ((AppCompatActivity) context).findViewById(toolbarId);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.toolbar.setTitle(title);
+        }
+    }
+
+    /**
+     * Shows toolbar at the top
+     */
+    void show() {
+        ((AppCompatActivity) context).setSupportActionBar(this.toolbar);
+    }
+
+    /**
+     * Adds go back arrow button
+     */
+    void addBackButton() {
+        show();
+        if (((AppCompatActivity) this.context).getSupportActionBar() != null) {
+            ((AppCompatActivity) this.context).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            ((AppCompatActivity) this.context).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+}
