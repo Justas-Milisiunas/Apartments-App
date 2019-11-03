05e60d759e975ad455fcabdd9b4715b575695e8f
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-03 15:00:24 +0200

    Added description property

--- app/src/main/java/com/apartmentslt/apartments/models/Apartment.java
+++ app/src/main/java/com/apartmentslt/apartments/models/Apartment.java
@@ -1,22 +1,26 @@
 package com.apartmentslt.apartments.models;
 
+import java.io.Serializable;
 import java.sql.Date;
 import java.time.Instant;
 
-public class Apartment {
+public class Apartment implements Serializable {
     private int size;
     private int roomsCount;
     private Date addedDate;
     private double price;
     private String address;
     private ApartmentStatus status;
+    private String description;
 
-    public Apartment(int size, int roomsCount, double price, String address, ApartmentStatus status) {
+    public Apartment(int size, int roomsCount, double price, String address, ApartmentStatus status,
+                     String description) {
         this.size = size;
         this.roomsCount = roomsCount;
         this.price = price;
         this.address = address;
         this.status = status;
+        this.description = description;
         this.addedDate = new Date(System.currentTimeMillis());
     }
 
@@ -42,5 +46,9 @@ public class Apartment {
 
     public ApartmentStatus getStatus() {
         return status;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
