b0a432123fa4f27658761373a5f5028de5348539
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-03 12:40:17 +0200

    Added navigation from login activity to the main activy based on user's role

--- app/src/main/java/com/apartmentslt/apartments/LoginActivity.java
+++ app/src/main/java/com/apartmentslt/apartments/LoginActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        this.email = findViewById(R.id.emailAddress);
+        this.password = findViewById(R.id.password);
 
         Appbar toolbar = new Appbar(this, R.id.toolbar, getTitle().toString());
         toolbar.show();
@@ -21,5 +26,25 @@ public class LoginActivity extends AppCompatActivity {
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    public void login(View view) {
+        switch (email.getText().toString().toLowerCase()) {
+            case "owner":
+                // TODO: Start owner main activity
+
+                finish();
+                break;
+            case "tenant" :
+                Intent intent = new Intent(this, ApartmentsListActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case "worker" :
+                // TODO: Start worker main activity
+
+                finish();
+                break;
+        }
     }
 }
