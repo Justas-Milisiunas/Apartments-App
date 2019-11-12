3631ee7fec1b3d1dd8cb73356ad12e1e33e6bc24
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-12 12:05:27 +0200

    Created write complaint activity with basic complaint writing form. Without functionality

---
+++ app/src/main/java/com/apartmentslt/apartments/tenant/activities/WriteComplaintActivity.java
@@ -0,0 +1,53 @@
+package com.apartmentslt.apartments.tenant.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.apartmentslt.apartments.Appbar;
+import com.apartmentslt.apartments.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class WriteComplaintActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_complaint);
+
+        // Adds toolbar
+        Appbar toolbar = new Appbar(this, R.id.toolbar, getTitle().toString());
+        toolbar.addBackButton();
+        toolbar.show();
+
+        // Adds bottom navigation bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_toolbar);
+        if (bottomNavigationView != null) {
+            bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        } else  {
+            Toast.makeText(this, "Bottom navigation bar could not be loaded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Bottom navigation bar clicked menu items listener
+     *
+     * @param menuItem Clicked menu item
+     * @return true
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_apartments_list:
+                finish(); // Closes active activity
+                break;
+            case R.id.navigation_write_complaint:
+                break;
+        }
+        return false;
+    }
+}
