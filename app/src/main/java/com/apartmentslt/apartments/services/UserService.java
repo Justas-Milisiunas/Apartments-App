55fa04e4a70b2afcf1dec10f1b3d8f6a2221ff0b
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-30 12:10:53 +0200

    Created service for handling request calls to API users controller

---
+++ app/src/main/java/com/apartmentslt/apartments/services/UserService.java
@@ -0,0 +1,13 @@
+package com.apartmentslt.apartments.services;
+
+import com.apartmentslt.apartments.models.User;
+import com.apartmentslt.apartments.models.UserRegistrationDto;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UserService {
+    @POST("api/users/register")
+    Call<User> register(@Body UserRegistrationDto user);
+}
