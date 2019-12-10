5e0b59dbad269eaceca21537c550ad804c22629d
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-12-11 01:13:05 +0200

    Created layout for editing profile

--- app/src/main/java/com/apartmentslt/apartments/profile/activities/ProfileEditActivity.java
+++ app/src/main/java/com/apartmentslt/apartments/profile/activities/ProfileEditActivity.java
@@ -3,9 +3,11 @@ package com.apartmentslt.apartments.profile.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.apartmentslt.apartments.Appbar;
 import com.apartmentslt.apartments.R;
+import com.apartmentslt.apartments.models.User;
 
 public class ProfileEditActivity extends AppCompatActivity {
 
@@ -16,5 +18,23 @@ public class ProfileEditActivity extends AppCompatActivity {
 
         Appbar toolbar = new Appbar(this, R.id.toolbar, getTitle().toString());
         toolbar.show();
+        toolbar.addBackButton();
+
+        bindData();
+    }
+
+    /**
+     * Binds logged in user's data to layout's components
+     */
+    private void bindData() {
+        EditText firstName = findViewById(R.id.firstName);
+        EditText lastName = findViewById(R.id.lastName);
+        EditText emailAddress = findViewById(R.id.emailAddress);
+
+        User user = User.getInstance();
+
+        firstName.setText(user.getVardas());
+        lastName.setText(user.getPavarde());
+        emailAddress.setText(user.getElPastas());
     }
 }
